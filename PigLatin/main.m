a0784989b1df50a0ccad55d7821363d760e1bef0
WaterBleu <jeff7746@hotmail.com>
2015-07-03 08:55:25 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Jeff Huang on 2015-07-03.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
